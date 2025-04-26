c624347b8d9833b74fb49218cf7980510ec22f49
ankitjhagithub21 <ajha45075@gmail.com>
2025-04-26 16:16:09 +0530

    minor changes

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -6,14 +6,17 @@ const UserInfo = () => {
     const { user } = useContext(AuthContext)
     return (
         <div className=" rounded-lg lg:w-[22%] w-full overflow-hidden border border-gray-300 bg-white">
-            <div className="h-20 bg-gray-500 w-full">
+            <div className="h-20 bg-gray-500 w-full overflow-hidden">
+                {
+                    user.coverImage && <img src={user.coverImage} alt="cover" className="w-full h-full object-cover" />
+                }
             </div>
             <img src={user.profilePhoto ? user.profilePhoto : "https://cdn-icons-png.flaticon.com/512/149/149071.png"} alt="" className="w-16 h-16 object-cover rounded-full -mt-8 ml-5" />
             <h1 className="mt-3 font-medium text-xl ml-5">{user.fullName}</h1>
-            <div className="p-3 text-sm">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, ex.</p>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem, ipsum dolor.</p>
+            <div className="ml-5 mb-5 text-gray-700 text-sm">
+                <p>{user.headline}</p>
+                <p>{user.location}</p>
+                <p>{user.position}</p>
             </div>
 
         </div>
